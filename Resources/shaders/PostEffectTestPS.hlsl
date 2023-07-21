07ef0e5fc1aae1e5b9825b6cac4f138da6c76795
watanaberaiki <k021g1324@g.neec.ac.jp>
2023-07-21 14:27:55 +0900

    ポストエフェクト基礎完成

--- Resources/shaders/PostEffectTestPS.hlsl
+++ Resources/shaders/PostEffectTestPS.hlsl
@@ -5,7 +5,20 @@ SamplerState smp : register(s0);
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	float4 texcolor=tex.Sample(smp,input.uv);
+	//float4 texcolor=tex.Sample(smp,input.uv);
+
+	//uv‚¸‚ç‚µ
+	float2 uv=(0,0);
+	float4 texcolor=tex.Sample(smp,input.uv+uv);
+
+
+	//–¾“x‚Ì•ÏX
+	//return float4(texcolor.rgb*2.0f,1);
+
+	////F”½“]
+	//float4 color=(255,255,255,1);
+	//return float4(color.rgb-texcolor.rgb,1);
 
 	return float4(texcolor.rgb,1);
+
 }
